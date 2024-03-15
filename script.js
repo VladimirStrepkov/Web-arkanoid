9c1343f687e3812df7763337270f4d448881a3cf
Vladimir Strepkov <MarsAlone@mail.ru>
2024-03-15 14:42:08 +0800

    Добавил класс игровых объектов и создал для них пять свойств с геттерами и сеттерами

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
+class gameObject {
+    #x;
+    #y;
+    #width;
+    #height;
+    #image;
+
+    constructor (x, y, width, height, image) {
+        this.#x = x;
+        this.#y = y;
+        this.#width = width;
+        this.#height = height;
+        this.#image = image;
+    }
+
+    get getX() {
+        return this.#x;
+    }
+    set setX(value) {
+        this.#x = value;
+    }
+
+    get getY() {
+        return this.#y;
+    }
+    set setY(value) {
+        this.#y = value;
+    }
+
+    get getWidth() {
+        return this.#width;
+    }
+
+    get getHeight() {
+        return this.#height;
+    }
+
+    get getImage() {
+        return this.#image;
+    }
+}
 
 
+
+// player = new gameObject(1, 2, 10, 10, 'sdfsf');
+// player.setX = 4;
+// console.log(player.getImage);
 
 // ctx.strokeStyle = 'red';
 // ctx.strokeRect(100, 100, 50, 50);
