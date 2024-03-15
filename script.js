0ed5452d10c238a98e601c62f21b89bf75317806
Vladimir Strepkov <MarsAlone@mail.ru>
2024-03-15 15:42:22 +0800

    Создал статический метод для класса игровых объектов отслеживающий пересечение двух игровых объектов

--- script.js
+++ script.js
@@ -41,6 +41,24 @@ class gameObject {
     get getImage() {
         return this.#image;
     }
+
+    static rectIntersection(gameObject1, gameObject2) {
+        const x1 = gameObject1.getX
+        const y1 = gameObject1.getY
+        const w1 = gameObject1.getWidth
+        const h1 = gameObject1.getHeight
+
+        const x2 = gameObject2.getX
+        const y2 = gameObject2.getY
+        const w2 = gameObject2.getWidth
+        const h2 = gameObject2.getHeight
+
+        if (x2 + w2 < x1 || x2 > x1 + w1 || y2 + h2 < y1 || y2 > y1 + h1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
 
 
